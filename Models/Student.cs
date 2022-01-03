ad529dedcb83dcf6be46efbc0752710629ba67ac
Vatsal <vatsaltailot6500@gmail.com>
2022-01-03 22:47:17 +0530

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebApplication1.Models
+{
+    public partial class Student
+    {
+        public int StudenId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}
